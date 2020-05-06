946da91ebf565c69101ee16bf317888044976dfd
Unknown <ilan.bleyhman@gmail.com>
2020-05-06 18:24:12 +0300

    Comments
    Added explanation about variable meaning

--- snr_sim.m
+++ snr_sim.m
@@ -1,11 +1,11 @@
 figure;
 n_sample=100;
-r1=linspace(0,10,n_sample);
-r2=linspace(0.5,2,n_sample);
+r1=linspace(0,10,n_sample); % r1 is the ratio between the thermal + readout variance and the mean of measurements {x}
+r2=linspace(0.5,2,n_sample); % r2 is the ratio between the measurement of interes x_i and the mean of measurements {x}
 [R1,R2]=meshgrid(r1,r2);
 for N=3:100
-    gain=(N-1)^2*(R1+R2)./((2*N-3)*R1+(2*N^2-4*N)+(3-N)*R2);
-    gain(gain<1)=0;
+    gain=(N-1)^2*(R1+R2)./((2*N-3)*R1+(2*N^2-4*N)+(3-N)*R2); % ratio of SNR between new and old schemes
+    gain(gain<1)=0; % no improvement - not interesting
     imagesc(r1,r2,sqrt(gain));
     title(['SNR gain, N=', num2str(N)]);
     xlabel('$\sigma_{0}^2 / \langle x \rangle$','Interpreter','latex');
